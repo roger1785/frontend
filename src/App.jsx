8108f915114917daef18fb69aec3568bd5e0c507
roger17 <roger17@git.com>
2026-04-06 20:28:04 +0200

    commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  const name = "Roger";
+  const age = 30;
+  return (
+    <>
+      <p>Bienvenidos a React</p>
+      <h1>
+        Mi nombre es {name} y mi edad es {age}
+      </h1>
+      <h2>Descubriendo React</h2>
+    </>
+  );
+
+}
+
+export default App;
